192205e4a53fa06437d0fc9351a41242088a352d
Lesley Cordero <lc2958@columbia.edu>
2015-07-20 15:37:28 -0400

    Update videoeditor.py

--- videoeditor.py
+++ videoeditor.py
@@ -1,15 +1,30 @@
+import moviepy.editor as mp
 from moviepy.editor import *
 import os
 import sys
 
-def callVideos(wildcard):
-    for item in os.listdir(wildcard):
-        #clip = VideoFileClip(item).subclip(50,60)
-        #clip.write_videofile("clip",fps=24, codec='mpeg4')
-        clip = VideoFileClip(item)
-        print(item)
-        print(clip.duration)
-        
+
+wildcard = './desktop/Videos'
+vid = './desktop/Videos/'
+for item in os.listdir(wildcard):
+        # figure out length of video
+
+    clip = VideoFileClip(vid + item)
+    dur = clip.duration
+    firstHalf = (dur/2.0) - 7.5
+    secHalf = (dur/2.0) + 7.5
+    end = dur - 15.0
+    listt = []
+    clip1 = clip.subclip(0, 15.0)
+    clip2 = clip.subclip(firstHalf, secHalf)
+    clip3 = clip.subclip(end, dur)
+    listt.append(clip1)
+    listt.append(clip2)
+    listt.append(clip3)
+    video = mp.concatenate(listt)
+    video.write_videofile(wildcard, fps=24, codec='mpeg4')
+    
+
 
 if __name__ == "__main__":
     wildcard = sys.argv[1]
